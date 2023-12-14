eace7dc79afbd1988d2836a4ace804efc964bd1c
JaeKyoung <worudeee@naver.com>
2023-12-14 22:40:14 +0900

    두 수 비교하기

---
+++ algorithm/baekjoon/step/two/두수비교하기.java
@@ -0,0 +1,16 @@
+package step.two;
+
+import java.util.Scanner;
+
+public class 두수비교하기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		if(A>B) System.out.println(">");
+		else if(A<B) System.out.println("<");
+		else System.out.println("==");
+	}
+}
