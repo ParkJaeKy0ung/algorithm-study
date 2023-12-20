9eaafb32fb0e1657ead6f4740d726b2e6379ae17
JaeKyoung <worudeee@naver.com>
2023-12-20 23:37:21 +0900

    알람시계

--- algorithm/baekjoon/step/two/알람시계.java
+++ algorithm/baekjoon/step/two/알람시계.java
@@ -1,7 +1,24 @@
 package step.two;
 
+import java.util.Scanner;
+
 public class 알람시계 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		int H = sc.nextInt();
+		int M = sc.nextInt();
+		
+		if(M<45) {
+			H--;
+			M = 60-(45-M);
+			if(H<0) {
+				H = 23;
+			}
+			System.out.println(H+" "+M);
+		}
+		else {
+			System.out.println(H+" "+(M-45));
+		}
 	}
 }
