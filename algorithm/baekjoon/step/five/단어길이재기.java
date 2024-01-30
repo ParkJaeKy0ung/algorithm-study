89419671d469ecbe337e5ca401c99c41a7b0ad19
JaeKyoung <worudeee@naver.com>
2024-01-30 23:32:30 +0900

    단어 길이 재기

---
+++ algorithm/baekjoon/step/five/단어길이재기.java
@@ -0,0 +1,11 @@
+package step.five;
+
+import java.util.Scanner;
+
+public class 단어길이재기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String st = sc.next();
+		System.out.println(st.length());
+	}	
+}
