abeaf6ad5639095a7465c1ae77e841d87419aaae
JaeKyoung <worudeee@naver.com>
2024-02-01 12:55:33 +0900

    아스키 코드

---
+++ algorithm/baekjoon/step/five/아스키코드.java
@@ -0,0 +1,10 @@
+package step.five;
+
+import java.util.Scanner;
+
+public class 아스키코드 {
+	public static void main(String[] args) throws Exception{
+		int a = System.in.read();
+		System.out.println(a);
+	}
+}
