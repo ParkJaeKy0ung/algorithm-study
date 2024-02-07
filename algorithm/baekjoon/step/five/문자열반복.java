9a5be497c02b8d47352b763ee0923f9ef8027572
JaeKyoung <worudeee@naver.com>
2024-02-07 18:48:43 +0900

    문자열 반복 (Re(?))

--- algorithm/baekjoon/step/five/문자열반복.java
+++ algorithm/baekjoon/step/five/문자열반복.java
@@ -9,12 +9,12 @@ public class 문자열반복 {
 		int T = sc.nextInt();
 		
 		for(int i=0; i<T; i++) {
-			int R = sc.nextInt();
-			String S = sc.next();
+			int R = sc.nextInt(); // 각 문자를 R번 반복해
+			String S = sc.next(); // 문자열 S를 입력받은 후
 			
 			for(int j=0; j<S.length(); j++) {
 				for(int k=0; k<R; k++) {
-					System.out.print(j);
+					System.out.print(S.charAt(j));
 				}
 			}
 			System.out.println();
