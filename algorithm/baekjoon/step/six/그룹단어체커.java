a52f593b7a5de41930dc33524ccfa146797fa344
JaeKyoung <worudeee@naver.com>
2024-02-26 23:21:15 +0900

    그룹단어체커(1)

---
+++ algorithm/baekjoon/step/six/그룹단어체커.java
@@ -0,0 +1,12 @@
+package step.six;
+
+import java.util.Scanner;
+
+public class 그룹단어체커 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt(); // 단어의 개수
+		String str = sc.next(); // 단어
+	}
+}
