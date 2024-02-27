e086b101c83509737f324b79210cf7b027dc4bc9
JaeKyoung <worudeee@naver.com>
2024-02-27 22:01:46 +0900

    행렬덧셈(1)

---
+++ algorithm/baekjoon/step/seven/행렬덧셈.java
@@ -0,0 +1,14 @@
+package step.seven;
+
+import java.util.Scanner;
+
+public class 행렬덧셈 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		
+	}
+}
