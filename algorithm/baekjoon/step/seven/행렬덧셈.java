ce74ab0987298755e0fb715b056ff40b20f006a3
JaeKyoung <worudeee@naver.com>
2024-02-28 13:27:01 +0900

    행렬덧셈(2)

--- algorithm/baekjoon/step/seven/행렬덧셈.java
+++ algorithm/baekjoon/step/seven/행렬덧셈.java
@@ -9,6 +9,28 @@ public class 행렬덧셈 {
 		int n = sc.nextInt();
 		int m = sc.nextInt();
 		
+		int[][] arrA = new int[n][m];
+		int[][] arrB = new int[n][m];
 		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				arrA[i][j] = sc.nextInt();
+			}
+		}
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				arrB[i][j] = sc.nextInt();
+			}
+		}
+		
+		sc.close();
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				System.out.print(arrA[i][j] + arrB[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 }
