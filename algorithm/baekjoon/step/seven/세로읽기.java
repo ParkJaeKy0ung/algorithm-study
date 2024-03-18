17f145a6cbaf3215e488ff79e938fb6763b9b1bd
JaeKyoung <worudeee@naver.com>
2024-03-18 11:46:55 +0900

    세로읽기(last)

--- algorithm/baekjoon/step/seven/세로읽기.java
+++ algorithm/baekjoon/step/seven/세로읽기.java
@@ -7,24 +7,26 @@ public class 세로읽기 {
 		Scanner sc = new Scanner(System.in);
 		
 		// 5줄 총 15개 2차원 배열 선언
-		char[][] charArr = new char[5][15];
+		char[][] str = new char[5][15];
 		
 		// 입력받은 문자들 그대로 출력
-		for(int i=0; i<5; i++) {
-			String str = sc.next();
+		String input = "";
+		for(int i=0; i<str.length; i++) {
+			input = sc.next();
 			
-			for(int j=0; j<str.length(); j++) {
-				charArr[i][j] = str.charAt(j);
+			// 15개 문자 넣기
+			for(int j=0; j<input.length(); j++) {
+				str[i][j] = input.charAt(j);
 			}
 		}
 		
 		// 세로로 출력
 		for(int i=0; i<15; i++) {
 			for(int j=0; j<5; j++) {
-				if(charArr[j][i] == ' ' || charArr[j][i] == '\0') {
+				if(str[j][i] == '\0') { // 빈 문자열이면 출력하지 않음
 					continue;
 				}
-				System.out.println(charArr[j][i]);
+				System.out.print(str[j][i]);
 			}
 		}
 	}
