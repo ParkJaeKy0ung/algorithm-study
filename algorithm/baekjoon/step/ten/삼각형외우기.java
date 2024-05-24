72d981088a1d4e393f50e4da69486801b3766bba
JaeKyoung <worudeee@naver.com>
2024-05-24 14:34:13 +0900

    삼각형 외우기

--- algorithm/baekjoon/step/ten/삼각형외우기.java
+++ algorithm/baekjoon/step/ten/삼각형외우기.java
@@ -9,5 +9,15 @@ public class 삼각형외우기 {
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		int c = sc.nextInt();
+		
+		if(a+b+c == 180) {
+			if(a==b && b==c) System.out.println("Equilateral");
+			else if(a==b || a==c || b==c) System.out.println("Isosceles");
+			else System.out.println("Scalene");
+		}
+		
+		else {
+			System.out.println("Error");
+		}
 	}
 }
