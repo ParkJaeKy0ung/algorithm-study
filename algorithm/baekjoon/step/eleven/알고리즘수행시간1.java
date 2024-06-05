17fc57746ad50264c2ea10e0f7c6ad4f7b4a5e7a
JaeKyoung <worudeee@naver.com>
2024-06-05 11:08:37 +0900

    알고리즘의 수행 시간 1

--- algorithm/baekjoon/step/eleven/알고리즘수행시간1.java
+++ algorithm/baekjoon/step/eleven/알고리즘수행시간1.java
@@ -4,10 +4,7 @@ import java.util.Scanner;
 
 public class 알고리즘수행시간1 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		
-		
+		System.out.println('1');
+		System.out.println('0');
 	}
 }
